db519475235b405eba8e492f6f9a9017c37e844d
Dimitry Lenchevsky <user@MacBook-Pro-user.local>
2019-11-17 12:57:26 +0300

    lesson1

--- triangle_square.rb
+++
@@ -1,8 +0,0 @@
-puts "Введите основание треугольника base:"
-base = gets.chomp.to_f
-
-puts "Введите высоту треугольника height:"
-height = gets.chomp.to_f
-
-area = 0.5 * base * height
-puts "Площадь треугольника равна 1/2*a*h: #{area}"
