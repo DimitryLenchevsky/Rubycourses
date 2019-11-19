fea5e8034f4b40f7203c795aa4fdc0aa718fd140
Dimitry Lenchevsky <user@MacBook-Pro-user.local>
2019-11-19 17:15:08 +0300

    add lesson 2

---
+++ lesson2/lesson2ex4.rb
@@ -0,0 +1,7 @@
+vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+vowels_hash = {}
+('a'..'z').each.with_index(1) do |letter, index|
+    vowels_hash[letter] = index if vowels.include?(letter)
+end
+
+puts vowels_hash
