1125798eb14e688babe7c001a7891d2921c8399f
Dimitry Lenchevsky <user@MacBook-Pro-user.local>
2019-11-19 17:22:04 +0300

    del all

--- lesson2/lesson2ex1.rb
+++
@@ -1,4 +0,0 @@
-year = {january: 31, february: 28, march: 31, april: 30, may: 31, june: 30, july: 31, august: 31, september: 30, october: 31, november: 30, december: 31}
-year.each do |months, days|
-    puts months if days == 30
-end
